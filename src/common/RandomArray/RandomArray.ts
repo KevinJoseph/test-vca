7c2beb43570bf76bbbd50ddbc731368f54f21d62
KevinJoseph <kevramos95@gmail.com>
2022-11-20 15:34:05 -0500

    fix: implements new method for validate rules

--- src/common/RandomArray/RandomArray.ts
+++ src/common/RandomArray/RandomArray.ts
@@ -1,6 +1,7 @@
 import { RandomArrayInterface } from './RandomArrayInterface';
 import { filteredArray } from '../helper/filteredArray';
 import { ItemInterface } from '../../schema/ItemInterface';
+import { isDeepStrictEqual } from 'util';
 
 export class RandomArray implements RandomArrayInterface {
   public array: any;
@@ -20,21 +21,62 @@ export class RandomArray implements RandomArrayInterface {
   }
 
   calculatePercentage() {
-    const percentageItem = 1 / this.array.length;
+    const percentageItem = (1 / this.array.length) * 100;
     return percentageItem;
   }
 
   chooseArrayRandom() {
     this.array.map((name: any, index: any) => {
       this.newArray.push({
-        index: index,
         name: name,
         percentage: this.percentageItem,
       });
     });
+
+    const resultArray = this.validateRules(this.newArray);
+    console.log(resultArray);
     return {
       itemRandom: this.chooseItemRandom(),
-      inputArray: this.newArray,
+      inputArray: resultArray,
     };
+  }
+
+  validateRules(array: any) {
+    const greaterThanOneHundred = this.validateGreaterThanOneHundred(this.newArray);
+    if (greaterThanOneHundred) {
+      return {
+        message: 'the percentage is greater than 100%',
+      };
+    }
+
+    return (this.array = this.validateLessThanOneHundred(this.newArray));
+  }
+
+  validateGreaterThanOneHundred(newArray: any) {
+    let total_percentage = 0;
+    newArray.map((item: any) => {
+      total_percentage = total_percentage + item.percentage;
+    });
+    if (total_percentage > 100) {
+      return true;
+    }
+  }
+  validateLessThanOneHundred(newArray: any) {
+    let total_percentage = 0;
+    newArray.map((item: any) => {
+      total_percentage = total_percentage + item.percentage;
+    });
+
+    if (total_percentage < 100) {
+      const difference_percentage = 100 - total_percentage;
+      const indeterminado = {
+        name: 'indeterminado',
+        percentage: difference_percentage,
+      };
+      this.newArray.push(indeterminado);
+      return this.newArray;
+    }
+
+    return this.newArray;
   }
 }
