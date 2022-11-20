14f9b9cd4c751f56396f7e9098e3d3d926224350
KevinJoseph <kevramos95@gmail.com>
2022-11-20 14:38:56 -0500

    project: implement handler post return new array

--- src/routes/index.router.ts
+++ src/routes/index.router.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from 'express';
+import { RandomArray } from '../common/RandomArray/RandomArray';
 
 class IndexRouter {
   router: Router;
@@ -10,6 +11,19 @@ class IndexRouter {
   routes() {
     this.router.get('/', (req: Request, res: Response) => {
       res.send('Hello');
+    });
+
+    this.router.post('/option', (req: Request, res: Response) => {
+      const array = req.body.data;
+
+      if (array.length === process.env.LIMIT_OPTION) {
+        res.send(`You exceeded the limit of options ${process.env.LIMIT_OPTION} allow.`);
+      }
+
+      const randomArray = new RandomArray(array);
+      const result = randomArray.chooseArrayRandom();
+
+      res.send(result);
     });
   }
 }
