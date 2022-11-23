a744224d53e7d613835c52a2b4e10dab84736962
KevinJoseph <kevramos95@gmail.com>
2022-11-23 16:14:29 -0500

    fix:refactor code

--- src/routes/User.router.ts
+++ src/routes/User.router.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express';
-import { UserController } from '../controllers/UserController';
+import { AuthController } from '../controllers/AuthController';
 
 export class UserRoutes {
   router: Router;
-  public userController: UserController = new UserController();
+  public authController: AuthController = new AuthController();
 
   constructor() {
     this.router = Router();
     this.routes();
   }
   routes() {
-    this.router.post('/register', this.userController.registerUser);
-    this.router.post('/login', this.userController.authenticateUser);
+    this.router.post('/signup', this.authController.signup);
+    this.router.post('/signin', this.authController.signin);
   }
 }
