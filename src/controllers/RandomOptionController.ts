868e8a7709d611a330227e51f5c38909db0b7d6f
KevinJoseph <kevramos95@gmail.com>
2022-11-23 16:12:27 -0500

    fix: add try catch to method chooseRandom

--- src/controllers/RandomOptionController.ts
+++ src/controllers/RandomOptionController.ts
@@ -6,15 +6,19 @@ export class RandomOptionController {
     return res.json('Please, you send a request post and add a body whit options');
   }
   async chooseRandomOption(req: Request, res: Response) {
-    const array = req.body.data;
+    try {
+      const array = req.body.data;
 
-    if (array.length === process.env.LIMIT_OPTION) {
-      res.send(`You exceeded the limit of options ${process.env.LIMIT_OPTION} allow.`);
+      if (array.length === process.env.LIMIT_OPTION) {
+        res.send(`You exceeded the limit of options ${process.env.LIMIT_OPTION} allow.`);
+      }
+
+      const randomArray = new RandomArray(array);
+      const result = randomArray.chooseArrayRandom();
+
+      res.json(result);
+    } catch (error) {
+      res.status(500).json(error);
     }
-
-    const randomArray = new RandomArray(array);
-    const result = randomArray.chooseArrayRandom();
-
-    res.json(result);
   }
 }
